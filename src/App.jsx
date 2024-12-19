f6b6916d89983083053d7b34faf6838b120de2a6
josephr04 <jrosas.dev@gmail.com>
2024-12-18 23:08:45 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TodoList from './components/TodoList.jsx'
+
+function App() {
+  return (
+    <>
+      <TodoList />
+      <input type="text" />
+      <button>Clear</button>
+    </>
+  )
+}
+
+export default App
