a498855f4797b32ff5ced01b80f1afae65a1eaf1
josephr04 <jrosas.dev@gmail.com>
2024-12-25 23:37:08 -0500

    Add not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -9,8 +9,12 @@ export function NotFound() {
     }, []);
 
     return (
-        <div className="bg-slate-950 min-h-screen text-white p-4">
-            <h1 className="text-3xl font-bold">Not Found</h1>
+        <div className="flex flex-col justify-center items-center bg-slate-950 min-h-screen text-white p-4 ">
+            <svg  xmlns="http://www.w3.org/2000/svg"  width="200"  height="200"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-mood-sad"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M9 10l.01 0" /><path d="M15 10l.01 0" /><path d="M9.5 15.25a3.5 3.5 0 0 1 5 0" /></svg>            <h1 className="text-6xl text-center font-bold">404</h1>
+            <h1 className="text-2xl text-center font-bold">Page not found</h1>
+            <button className='flex bg-sky-400 rounded-md mt-4 py-1 px-4'>
+                Go Home
+            </button>
       </div>
     );
 }
