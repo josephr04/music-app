8c84335ac86f1e0d81d164b3f3f41d3704084d68
josephr04 <jrosas.dev@gmail.com>
2024-12-27 23:00:13 -0500

    Fix quick picks section

--- src/components/QuickPicks.jsx
+++ src/components/QuickPicks.jsx
@@ -8,9 +8,9 @@ export function QuickPicks() {
     return (
         <>
             <h1 className="text-white text-[1.44rem] p-4 ml-2 text-left font-semibold">Quick Picks</h1>
-            <div className='w-full px-6 mb-8 grid grid-flow-col grid-rows-4 overflow-x-auto whitespace-nowrap gap-4 gap-x-20'>
+            <div className='w-full px-6 mb-8 grid grid-flow-col grid-rows-4 overflow-x-auto whitespace-nowrap gap-4'>
                 {songs.map((song) => (
-                    <div key={song.id} className="flex items-center w-64 cursor-pointer space-x-4 mr-6">
+                    <div key={song.id} className="flex items-center w-64 cursor-pointer space-x-4 mr-24">
                         <div className="w-16 h-16 flex-shrink-0 rounded-lg bg-green-500 hover:bg-slate-700 shadow-md transition-all"></div>
                         <div className='flex flex-col'>
                             <h1 className="text-left text-white text-base font-medium w-44 overflow-hidden text-ellipsis">{song.name}</h1>
