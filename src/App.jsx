4e06cc8632a8b83c5511747b864bb70c2bef9ffe
josephr04 <jrosas.dev@gmail.com>
2024-12-28 23:41:18 -0500

    Fix App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { AppRoutes } from './routes';
+import { MusicProvider } from '@context/MusicContext';
 
 export function App() {
   return (
-    <>
+    <MusicProvider>
       <AppRoutes />
-    </>
+    </MusicProvider>
   );
 }
