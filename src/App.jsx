21e4bfe4b0fa1baf7c1dbe8d119200435e33cf1c
josephr04 <jrosas.dev@gmail.com>
2024-12-30 17:30:55 -0500

    Setting loading screen on app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { AppRoutes } from './routes';
 import { MusicProvider } from '@context/MusicContext';
+import { LoadingScreen } from '@components/LoadingScreen';
 
 export function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadData = async () => {
+      await new Promise(resolve => setTimeout(resolve, 3000));
+      setLoading(false);
+    };
+
+    loadData();
+  }, []);
+
   return (
     <MusicProvider>
-      <AppRoutes />
+      {loading ? <LoadingScreen /> : <AppRoutes />}
     </MusicProvider>
   );
 }
