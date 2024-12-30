d5315c53322e005712089a65fe82b7e54018fbc1
josephr04 <jrosas.dev@gmail.com>
2024-12-29 21:52:26 -0500

    Fix routes file

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,16 +1,31 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { MainLayout } from '@components/layout/MainLayout';
+import { useEffect, useState } from 'react';
+import { useMusic } from '@context/MusicContext';
 import { Home } from '@pages/Home';
 import { MixPage } from '@pages/MixPage';
 import { NotFound } from '@pages/NotFound';
 
 export const AppRoutes = () => {
+  const { mixes } = useMusic();
+  const [dynamicRoutes, setDynamicRoutes] = useState([]);
+
+  useEffect(() => {
+    const routes = mixes.map((mix) => ({
+      path: `/playlist/${mix.id}`,
+      element: <MixPage mix={mix} />
+    }));
+    setDynamicRoutes(routes);
+  }, [mixes]);
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainLayout />}>
             <Route index element={<Home />} />
-            <Route path="playlist/:mixName" element={<MixPage />} />
+            {dynamicRoutes.map((route) => (
+              <Route key={route.path} path={route.path} element={route.element} />
+            ))}
             <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
