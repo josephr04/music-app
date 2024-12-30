a407879c31122507482c9ad1f2aa82fbc90829f7
josephr04 <jrosas.dev@gmail.com>
2024-12-29 21:54:09 -0500

    Adding images and fixing vite config file

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,10 @@ export default defineConfig({
       '@components': path.resolve(__dirname, 'src/components'),
       '@assets': path.resolve(__dirname, 'src/assets'),
       '@pages': path.resolve(__dirname, 'src/pages'),
+      '@config': path.resolve(__dirname, 'src/config'),
+      '@api': path.resolve(__dirname, 'src/api'),
+      '@context': path.resolve(__dirname, 'src/context'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
     }
   },
 })
