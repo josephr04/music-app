603e6e6eabc735a12c1d3d2884b34a52398a59bd
josephr04 <jrosas.dev@gmail.com>
2025-01-03 21:13:04 -0500

    Add logos animation

---
+++ src/components/Logos.jsx
@@ -0,0 +1,15 @@
+export function ReactLogo() {
+    return (
+        <div className="container">
+            <span className="react-logo">
+                <span className="nucleo"></span>
+            </span>
+        </div>
+    );
+}
+
+export function TailwindLogo() {
+    return(
+        <div className="tailwind-logo"></div>
+    );
+}
