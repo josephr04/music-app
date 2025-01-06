f9d8a1d4d2a4265849052942f98a0caf79efc7ac
josephr04 <jrosas.dev@gmail.com>
2025-01-05 20:19:39 -0500

    Fix player bar and quick picks section

--- src/components/PlayerBar.jsx
+++ src/components/PlayerBar.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Slider } from '@components/Slider'
 import { useMusic } from '@context/MusicContext';
 import { PlayPauseButton } from '@components/PlayPauseButton';
+import { VolumeButton } from '@components/VolumeButton';
 
 export function PlayerBar() {
   const [expanded, setExpanded] = useState(false);
@@ -52,8 +53,8 @@ export function PlayerBar() {
                   mode="mini"
                 />
               </div>
-              <div className='flex space-x-8 items-center justify-center mt-4'>
-                <div onClick={togglePlayerBar} className='flex space-x-6 pr-12'>
+              <div className='flex space-x-8 md:space-x-0 items-center justify-center md:justify-between mt-4 md:mx-4'>
+                <div onClick={togglePlayerBar} className='flex space-x-6 pr-12 md:pr-0'>
                   <div className="w-12 h-12 flex-shrink-0 rounded-md shadow-md transition-all cursor-pointer"
                     style={{
                       backgroundImage: `url(${currentSong.image})`,
@@ -63,16 +64,37 @@ export function PlayerBar() {
                   >
                   </div>
                   <div className='flex flex-col cursor-pointer w-44'>
-                      <h1 className="text-left text-white text-base font-medium w-44 overflow-hidden whitespace-nowrap text-ellipsis">{currentSong.title}</h1>
-                      <h2 className="text-left text-slate-500 text-base w-44 overflow-hidden whitespace-nowrap text-ellipsis">{currentSong.artist}</h2>
+                      <h1 className="text-left text-white text-base font-medium w-44 md:w-56 overflow-hidden whitespace-nowrap text-ellipsis">{currentSong.title}</h1>
+                      <h2 className="text-left text-slate-500 text-base w-44 md:w-56 overflow-hidden whitespace-nowrap text-ellipsis">{currentSong.artist}</h2>
                   </div>
                 </div>
-                <div>
-                  <PlayPauseButton expanded={expanded} fill="white" iconSize={24} 
-                    onToggle={(e) => {
-                      e.stopPropagation();
-                    }}
-                  />
+                <div className='md:flex md:space-x-4 md:pr-[2%]'>
+                  {/* Previous button */}
+                  <div onClick={playPrevious} className='hidden md:flex items-center justify-center'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="white" className="icon icon-tabler icons-tabler-filled icon-tabler-player-skip-back cursor-pointer"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.496 4.136l-12 7a1 1 0 0 0 0 1.728l12 7a1 1 0 0 0 1.504 -.864v-14a1 1 0 0 0 -1.504 -.864z" /><path d="M4 4a1 1 0 0 1 .993 .883l.007 .117v14a1 1 0 0 1 -1.993 .117l-.007 -.117v-14a1 1 0 0 1 1 -1z" /></svg>
+                  </div>
+                  {/* play/pause button */}
+                  <div className=''>
+                    <PlayPauseButton expanded={expanded} fill="white" iconSize={24} 
+                      onToggle={(e) => {
+                        e.stopPropagation();
+                      }}
+                    />
+                  </div>
+                  {/* Next button */}
+                  <div onClick={playNext} className='hidden md:flex items-center justify-center'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="white" className="icon icon-tabler icons-tabler-filled icon-tabler-player-skip-forward cursor-pointer"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 5v14a1 1 0 0 0 1.504 .864l12 -7a1 1 0 0 0 0 -1.728l-12 -7a1 1 0 0 0 -1.504 .864z" /><path d="M20 4a1 1 0 0 1 .993 .883l.007 .117v14a1 1 0 0 1 -1.993 .117l-.007 -.117v-14a1 1 0 0 1 1 -1z" /></svg>                
+                  </div>
+                </div>
+                <div className='hidden md:flex md:items-center md:space-x-8'>
+                  {/* Volume Button */}
+                  <div >
+                    <VolumeButton />
+                  </div>
+                  {/* Expand button */}
+                  <div onClick={togglePlayerBar}>
+                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="#bdb8b7"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-arrows-maximize cursor-pointer hover:stroke-white"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M16 4l4 0l0 4" /><path d="M14 10l6 -6" /><path d="M8 20l-4 0l0 -4" /><path d="M4 20l6 -6" /><path d="M16 20l4 0l0 -4" /><path d="M14 14l6 6" /><path d="M8 4l-4 0l0 4" /><path d="M4 4l6 6" /></svg>
+                  </div>
                 </div>
               </div>
             </div>
@@ -83,7 +105,7 @@ export function PlayerBar() {
             <div className="w-full h-full flex flex-col items-center justify-center text-white">
               <button
                 onClick={togglePlayerBar}
-                className="absolute top-8 left-4 text-white text-2xl rounded-full p-2 hover:bg-gray-600 transition-all"
+                className="absolute md:hidden top-8 left-4 text-white text-2xl rounded-full p-2 hover:bg-gray-600 transition-all"
               >
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-down"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 9l6 6l6 -6" /></svg>
               </button>
@@ -116,17 +138,23 @@ export function PlayerBar() {
               </div>
               {/* Control panel*/}
               <div className="flex items-center space-x-4 mt-4">
-                {/* Back button */}
-                <div onClick={playPrevious} className='flex items-center justify-center w-14 h-14 active:bg-slate-800 rounded-full'>
+                {/* Previous button */}
+                <div onClick={playPrevious} className='flex items-center justify-center w-14 h-14 active:bg-slate-800 rounded-full cursor-pointer md:hover:bg-slate-800'>
                   <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="currentColor" className="icon icon-tabler icons-tabler-filled icon-tabler-player-skip-back"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.496 4.136l-12 7a1 1 0 0 0 0 1.728l12 7a1 1 0 0 0 1.504 -.864v-14a1 1 0 0 0 -1.504 -.864z" /><path d="M4 4a1 1 0 0 1 .993 .883l.007 .117v14a1 1 0 0 1 -1.993 .117l-.007 -.117v-14a1 1 0 0 1 1 -1z" /></svg>
                 </div>
                 {/* play/pause button */}
-                <div className='flex items-center justify-center w-20 h-20 bg-white rounded-full'>
+                <div className='flex items-center justify-center w-20 h-20 bg-white rounded-full cursor-pointer'>
                   <PlayPauseButton expanded={expanded} fill="black" iconSize={36} onToggle={(e) => e.stopPropagation()} />
                 </div>
                 {/* Next button */}
-                <div onClick={playNext} className='flex items-center justify-center w-14 h-14 active:bg-slate-800 rounded-full'>
+                <div onClick={playNext} className='flex items-center justify-center w-14 h-14 active:bg-slate-800 rounded-full cursor-pointer md:hover:bg-slate-800'>
                   <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="currentColor" className="icon icon-tabler icons-tabler-filled icon-tabler-player-skip-forward"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 5v14a1 1 0 0 0 1.504 .864l12 -7a1 1 0 0 0 0 -1.728l-12 -7a1 1 0 0 0 -1.504 .864z" /><path d="M20 4a1 1 0 0 1 .993 .883l.007 .117v14a1 1 0 0 1 -1.993 .117l-.007 -.117v-14a1 1 0 0 1 1 -1z" /></svg>                
+                </div>
+              </div>
+              <div className='hidden md:flex md:ml-auto md:mr-8 md:mt-4 md:space-x-8'>
+                <VolumeButton />
+                <div className='' onClick={togglePlayerBar}>
+                  <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="#bdb8b7"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-arrows-diagonal-minimize-2 cursor-pointer hover:stroke-white"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 10h-4v-4" /><path d="M20 4l-6 6" /><path d="M6 14h4v4" /><path d="M10 14l-6 6" /></svg>
                 </div>
               </div>
             </div>
--- src/components/QuickPicks.jsx
+++ src/components/QuickPicks.jsx
@@ -1,19 +1,76 @@
 import { useMusic } from '@context/MusicContext';
+import { useRef, useState } from 'react';
 
 export function QuickPicks() {
-    const { songs, cloudId, playSong } = useMusic();
+    const { songs, cloudId, playSong, deskMenuOpen } = useMusic();
+    const scrollContainerRef = useRef(null);
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const itemsPerScroll = 5;
+    const maxScrollLimit = 3;
+
+    const scroll = (direction) => {
+        if (scrollContainerRef.current) {
+            const container = scrollContainerRef.current;
+            const itemWidth = container.scrollWidth / songs.length;
+            let newIndex = currentIndex;
+
+            if (direction === 'left') {
+                newIndex = Math.max(currentIndex - itemsPerScroll, 0);
+            } else if (direction === 'right') {
+                newIndex = Math.min(currentIndex + itemsPerScroll, maxScrollLimit * itemsPerScroll);
+            }
+
+            setCurrentIndex(newIndex);
+
+            container.scrollTo({
+                left: newIndex * itemWidth,
+                behavior: 'smooth',
+            });
+        }
+    };
 
     return (
         <>
-            <h1 className="text-white text-[1.44rem] p-4 ml-2 text-left font-semibold">Quick Picks</h1>
-            <div className='w-full px-20 mb-8 grid grid-flow-col grid-rows-4 overflow-x-auto snap-x snap-mandatory scroll-smooth whitespace-nowrap gap-4'>
-                {songs.map((song) => (
-                    <div key={song.id} onClick={() => playSong(song.file, song.title, song.artist, song.image)} className="snap-start flex items-center cursor-pointer space-x-4 mr-24">
-                        <div className="ml-7 w-16 h-16 flex-shrink-0 rounded-lg hover:bg-slate-700 shadow-md transition-all"
+            <div className='flex items-center'>
+                <h1 className="text-white text-[1.44rem] p-4 ml-2 text-left font-semibold">Quick Picks</h1>
+                <div className='hidden md:flex ml-auto mr-8 space-x-4'>
+                    <div
+                        onClick={() => scroll('left')}
+                        className={`p-1 rounded-full border-2 border-slate-600 ${currentIndex === 0 ? 'opacity-50 cursor-default' : 'hover:bg-slate-800 cursor-pointer'}`}
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M15 6l-6 6l6 6" />
+                        </svg>
+                    </div>
+                    <div
+                        onClick={() => scroll('right')}
+                        className={`p-1 rounded-full border-2 border-slate-600 ${currentIndex >= maxScrollLimit * itemsPerScroll ? 'opacity-50 cursor-default' : 'hover:bg-slate-800 cursor-pointer'}`}
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M9 6l6 6l-6 6" />
+                        </svg>
+                    </div>
+                </div>
+            </div>
+            <div
+                ref={scrollContainerRef}
+                className='w-full px-20 mb-8 grid grid-flow-col grid-rows-4 overflow-x-auto snap-x snap-mandatory scroll-smooth whitespace-nowrap gap-4 md:grid-rows-4 md:ml-4 md:gap-x-12 md:gap-y-0 md:h-96 md:px-8'
+            >
+                {songs.map((song, index) => (
+                    <div
+                        key={song.id}
+                        onClick={() => playSong(song.file, song.title, song.artist, song.image)}
+                        className={`snap-start flex items-center cursor-pointer space-x-4 mr-24 md:mr-0 md:p-2 ${!deskMenuOpen ? 'md:pr-44' : 'md:pr-28'} md:hover:bg-slate-800 md:rounded-md`}
+                    >
+                        <div
+                            className="ml-7 w-16 h-16 flex-shrink-0 rounded-lg hover:bg-slate-700 shadow-md transition-all md:ml-2"
                             style={{
                                 backgroundImage: `url(${cloudId + song.image})`,
                                 backgroundSize: 'cover',
-                                backgroundPosition: 'center'
+                                backgroundPosition: 'center',
                             }}
                         ></div>
                         <div className='flex flex-col w-36'>
