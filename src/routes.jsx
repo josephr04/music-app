73ada8f2a237e9749243c5120532893b3176630b
josephr04 <jrosas.dev@gmail.com>
2025-01-07 23:21:56 -0500

    Fixing routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { MainLayout } from '@components/layout/MainLayout';
 import { useEffect, useState } from 'react';
 import { useMusic } from '@context/MusicContext';
@@ -19,16 +19,14 @@ export const AppRoutes = () => {
   }, [mixes]);
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-            <Route index element={<Home />} />
-            {dynamicRoutes.map((route) => (
-              <Route key={route.path} path={route.path} element={route.element} />
-            ))}
-            <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        {dynamicRoutes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/music-app/',
   plugins: [react()],
     server: {
     port: 3000,
